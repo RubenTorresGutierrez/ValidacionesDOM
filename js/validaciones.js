77e1c69279dbe63eafcdb4203a3212f2d53fa5f5
RubenTorres <rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net>
2021-10-26 23:39:55 +0200

    Validaciones de campos de formulario

--- js/validaciones.js
+++ js/validaciones.js
@@ -21,28 +21,93 @@ function iniciar(){
 
 function validar(evento){
 
-    console.log('Estoy validando');
+    //
+    evento.preventDefault();
+
+    //Eliminar mensajes de error si existen
+    if(document.getElementsByClassName('mensajeerror')){
+        let elementos = document.getElementsByClassName('mensajeerror');
+        let inputs = document.getElementsByTagName('input');
+        let selects = document.getElementsByTagName('select');
+
+        for(let i = 0; i < elementos.length; i++)
+            if(elementos[i]){
+                elementos[i].remove();
+                i--;
+            }
+        for(let i = 0; i < inputs.length; i++)
+            inputs[i].style.borderColor = '#ccc';
+        for(let i = 0; i < selects.length; i++)
+            selects[i].style.borderColor = '#ccc';
+    }
 
     //Comprobar Aceptación de la Política de privacidad
-    if(document.getElementById('iPolitica').checked)
-        return false;
+    if(!document.getElementById('iPolitica').checked){
+        document.getElementById('iPolitica').style.borderColor = 'red';
+        crearMensaje('Debe aceptar la Política de Privacidad.', 8);
+    }
 
-    //Comprobar campo Nombre
-    if(document.getElementById('iNombre').length < 2)
-        return false;
+    //Comprobar Nombre
+    if(document.getElementById('iNombre').value.length < 3){
+        document.getElementById('iNombre').style.borderColor = 'red';
+        crearMensaje('Introduzca un nombre superior a 2 caracteres.', 0);
+    }
+
+    //Comprobar Apellidos
+    let palabras = document.getElementById('iApellidos').value.split(' ');
+    if(palabras.length < 2){
+        document.getElementById('iApellidos').style.borderColor = 'red';
+        crearMensaje('Introduzca dos apellidos.', 1);
+    }
+
+    //Comprobar NIF
+    if(!comprobarNif()){
+        document.getElementById('iNIF').style.borderColor = 'red';
+        crearMensaje('Introduzca un NIF válido.', 4);
+    }
+
+    //Comprobar Asturiano y brócoli
+    if(document.getElementById('rSi').checked && document.getElementById('sComunidad').value === '3'){
+        document.getElementById('sComunidad').style.borderColor = 'red';
+        crearMensaje('A mi no me engañas, todos sabemos que a los Asturianos no les gusta el brócoli.', 7);
+    }
+
+}
+
+function crearMensaje(mensaje, posicion){
+
+    let p = document.createElement('p');
+    let texto = document.createTextNode('* ' + mensaje);
+
+    p.appendChild(texto);
+    p.classList.add('mensajeerror');
+
+    document.getElementsByClassName('col-75')[posicion].appendChild(p);
+
+
+}
+
+function comprobarNif(){
     
-    //evento.preventDefault();
-
+    const letrasnif = "TRWAGMYFPDXBNJZSQVHLCKE";
+    let nif = document.getElementById('iNIF').value;
+    let numero = parseInt(nif.substring(0,8))%23;
+    let letra = nif.charAt(8);
+    
+    if(letra == letrasnif.charAt(numero))
+        return true;
+    return false;
+    
 }
 
 function selectProvincia(){
 
     if(document.getElementById('sComunidad').value == 11){
+
         //DIV class=row
         let row = document.createElement('div');
         row.classList.add('row');
         row.id = 'provincias';
-        //document.forms[0].appendChild(row);
         document.forms[0].insertBefore(row, document.getElementsByClassName('row')[7]);
 
         //DIV class=col-25
